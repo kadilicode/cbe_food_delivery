4e15a4db56f3328c0fbf351906c19d791401b5ae
kadilicode <kadiliy17@gmail.com>
2026-02-11 19:52:51 +0300

    Update config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@ session_start();
 $conn = mysqli_connect("localhost", "root", "", "kadili_food_db");
 
 // ZenoPay Config
-define('ZP_API_KEY', 'CR3eQVBuusoIF0vBTAyaKx5XnMmEwBI49zFPcpqqk3aZeV0ISW25l2DaFWcFJOvl_J1YJRcEKYoSdlZ8ZIa26Q');
+define('ZP_API_KEY', 'your API');
 define('ZP_BASE_URL', 'https://api.zenopay.com');
 
 function sanitize($data) {
